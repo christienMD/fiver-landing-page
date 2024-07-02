b76a5325e92aba873964371bc58000ab9707e576
Md Christien <mdchristien12@gmail.com>
2024-07-02 15:51:37 +0100

    Build The Fiverr Join section

--- app/components/GetHelp.tsx
+++ app/components/GetHelp.tsx
@@ -6,43 +6,49 @@ const GetHelp = () => {
   return (
     <section>
       <FiverrHeading label="Guides to help you grow" />
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-6 mt-10">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-4 mt-10">
         <div className="">
-          <div className="relative w-[267px] md:w-[300px] h-[186px] md:h-[210px]">
+          <div className="relative w-[267px] md:w-[290px] h-[186px] md:h-[200px] lg:w-[300px] lg:h-[250px] xl:w-[400px] xl:h-[300px] rounded-md">
             <Image
               alt=""
               src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_600,dpr_1.0/v1/attachments/generic_asset/asset/0c7c1b07050e6ea2a0901861b48b6264-1658846941284/side%20hustle.jpeg"
               fill
               sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-              className="object-cover"
+              className="object-cover rounded-md"
             />
           </div>
-          <p className="mt-2 text-zinc-700 font-semibold text-lg">Start a buisiness</p>
+          <p className="mt-2 text-zinc-700 font-semibold text-lg">
+            Start a buisiness
+          </p>
         </div>
         <div className="">
-          <div className="relative w-[267px] md:w-[300px] h-[186px] md:h-[210px]">
+          <div className="relative w-[267px] md:w-[290px] h-[186px] md:h-[200px] lg:w-[300px] lg:h-[250px] xl:w-[400px] xl:h-[300px] rounded-md">
             <Image
               alt=""
               src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_600,dpr_1.0/v1/attachments/generic_asset/asset/687d698a96f4eef875648319685ffeed-1687027332007/1685561103924-12profitableecommercebusinessideasyoucanstarttoday.jpg"
               fill
-              className="object-cover"
+              className="object-cover rounded-md"
               sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
             />
           </div>
-          <h3 className="mt-2 text-zinc-700 font-semibold text-lg">Start a buisiness</h3>
+          <h3 className="mt-2 text-zinc-700 font-semibold text-lg">
+            Start a buisiness
+          </h3>
         </div>
         <div className="">
-          <div className="relative w-[267px] md:w-[300px] h-[186px] md:h-[210px]">
+          <div className="relative w-[267px] md:w-[290px] h-[186px] md:h-[200px] lg:w-[300px] lg:h-[250px] xl:w-[400px] xl:h-[300px] rounded-md">
             <Image
               alt=""
               src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_600,dpr_1.0/v1/attachments/generic_asset/asset/5907f56b0e099c84efe5f480840f43a2-1593446948907/home%20based%20online%20business-fiverr.jpg"
               fill
-              className="object-cover"
+              className="object-cover rounded-md"
               sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
               quality={100}
             />
           </div>
-          <h3 className="mt-2 text-zinc-700 font-semibold text-lg">Start a buisiness</h3>
+          <h3 className="mt-2 text-zinc-700 font-semibold text-lg">
+            Start a buisiness
+          </h3>
         </div>
       </div>
     </section>
--- app/components/JoinFiverr.tsx
+++ app/components/JoinFiverr.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const JoinFiverr = () => {
   return (
-    <div className="bg-[#4d1727] text-white flex flex-col items-center gap-7 p-6 my-10 rounded-sm">
-      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center">
+    <div className="bg-[#4d1727] text-white flex flex-col items-center gap-7 px-6 py-8 my-10 rounded-lg">
+      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center lg:max-w-3xl">
         Freelance services at your <span className="text-[#ff7640]">fingertips!</span>
       </h2>
       <button className="inline-flex px-4 py-2 text-[#4d1727] bg-white rounded-md">
